a22e8f398b1e6b77606648c43a298a44cfc06794
Rhian McClelland <rhianmcclelland193@hotmail.com>
2024-02-21 06:24:29 +1000

    prepare test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="sietch",
-    version="0.2.0dev0",
+    version="0.2.0dev1",
     author="GandalfsDad",
     description="Lightweight environment & cli tool",
     long_description=long_description,
